5cb66ca6a74758efcbbd42b285c3a303a564502b
MKish420 <meaghanmpeters@hotmail.com>
2022-08-01 23:31:15 -0400

    first commit folder structure, json started, server connection schema seeds added needs routed and seeds

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const Routes = require('./Routes'); //add routes name 
+const Routes = require('./Routes');//add routes name 
+
+router.use('/', Routes); //add model name, route name 
+router.use('/', Routes); //add model name, route name 
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- label database run on dbeaver
+
+DROP DATABASE IF EXISTS _db;
+CREATE DATABASE _db;
